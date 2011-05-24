f97ddd17f7bfd66c6860bf4b7766d05909371f13
Drew Stokes <Drew@MacMini.local>
2011-05-24 16:49:43 -0700

    cleanup

--- kees.js
+++ kees.js
@@ -1,7 +1,37 @@
+/**
+* TODOS
+* Add remove function
+* Add keyup option
+* Improve pressed tracking
+* Simplify array comparison in arraysEqual
+* 5/24
+*/
 var Kees = (function() {
     var shortcuts = [],
         pressed = [],
-        ctrls = {'shift':16,'ctrl':17,'alt':18,'backspace':8,'tab':9,'enter':13,'pause':19,'capslock':20,'esc':27,'space':32,'pageup':33,'pagedown':34,'end':35,'home':36,'left':37,'up':38,'right':39,'down':40,'insert':45,'delete':46,'f1':112,'f2':113,'f3':114,'f4':115,'f5':116,'f6':117,'f7':118,'f8':119,'f9':120,'f10':121,'f11':122,'f12':123,'?':191};
+        ctrls = {
+	        'shift':16,
+	        'ctrl':17,
+	        'alt':18,
+	        'backspace':8,
+	        'tab':9,
+	        'enter':13,
+	        'pause':19,
+	        'capslock':20,
+	        'esc':27,
+		    'space':32,
+		    'pageup':33,
+		    'pagedown':34,
+		    'end':35,
+		    'home':36,
+		    'left':37,
+		    'up':38,
+		    'right':39,
+		    'down':40,
+		    'insert':45,
+		    'delete':46,
+		    '?':191
+		};
 
     //Setup observers
     document.addEventListener('keyup', keyUpHandler, false);
@@ -41,6 +71,7 @@ var Kees = (function() {
         pressed = p;
     }
     
+    //Check pressed against defined shortcuts
     function checkShorts(e) {
         for(var i = 0, l = shortcuts.length; i < l; i++) {
             var keys = shortcuts[i].keys;
@@ -51,6 +82,7 @@ var Kees = (function() {
         }
     }
     
+    //Compare pressed array and shortcut array
     function arraysEqual(a, b) {
         if(!a || !b) return false;
         if(a.length !== b.length) return false;
