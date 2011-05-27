65f66212eaffed9d2c86192f5624356ba7a72ffa
Drew Stokes <Drew@MacMini.local>
2011-05-27 12:58:13 -0700

    just some cleanup

--- kees.js
+++ kees.js
@@ -1,78 +1,57 @@
 /**
 * TODOS
-* Add remove function
 * Add keyup option
-* Improve pressed tracking
-* Simplify array comparison in arraysEqual
 * 5/24
 */
 var Kees = (function() {
     var shortcuts = [],
         pressed = [],
-        ctrls = {
-	        'shift':16,
-	        'ctrl':17,
-	        'alt':18,
-	        'backspace':8,
-	        'tab':9,
-	        'enter':13,
-	        'pause':19,
-	        'capslock':20,
-	        'esc':27,
-	        'space':32,
-	        'pageup':33,
-		    'pagedown':34,
-		    'end':35,
-		    'home':36,
-		    'left':37,
-		    'up':38,
-		    'right':39,
-		    'down':40,
-		    'insert':45,
-		    'delete':46,
-		    '?':191
-		};
+        ctrls = {'shift':16,'ctrl':17,'alt':18,'backspace':8,'tab':9,'enter':13,'pause':19,'capslock':20,'esc':27,'space':32,'pageup':33,'pagedown':34,'end':35,'home':36,'left':37,'up':38,'right':39,'down':40,'insert':45,'delete':46,'?':191};
 
     //Setup observers
     document.addEventListener('keyup', keyUpHandler, false);
     document.addEventListener('keydown', keyDownHandler, false);
 
-    //Add a key combo to the list
-    function add(def, handler) {
-        shortcuts.push({
-            keys: parseDef(def),
-            cb: handler
-        });
+	function add(def, handler) {
+        shortcuts.push({ def: def, keys: getKeys(def), cb: handler });
+    }
+    
+    function remove(def) {
+        for(var i = 0, p = [], l = shortcuts.length; i < l; i++)
+            if(def !== shortcuts[i].def) p.push(pressed[i]);
+        shortcuts = p;
     }
 
-    //Parse shortcut definition
-    function parseDef(def) {
-        var str = (def || '').split('+'),
-            keys = [];
-
-        for (var i = 0, l = str.length; i < l; i++) {
+	//Parse shortcut definition for keycodes
+    function getKeys(def) {
+        var str = (def || '').split('+');
+        for(var i = 0, keys = [], l = str.length; i < l; i++) {
+	        //Get keycode from `ctrls` or  w/ charCodeAt
             var code = str[i] in ctrls ? ctrls[str[i]] : str[i].toUpperCase().charCodeAt();
             keys.push(code);
         }
+        
+        //Sort for later arr comparison
         return keys.sort();
     }
 
+
     function keyDownHandler(e) {
-        if(!(~pressed.indexOf(e.keyCode)))
+	    //If the key isnt in the pressed array, add it
+        if(!~pressed.indexOf(e.keyCode))
             pressed.push(e.keyCode);
-        
-        checkShorts(e);
+        pressed.sort();
+        checkShortcuts(e);
     }
 
     function keyUpHandler(e) {
-        var p = [];
-        for(var i = 0, l = pressed.length; i < l; i++)
+        for(var i = 0, p = [], l = pressed.length; i < l; i++)
             if(pressed[i] != e.keyCode) p.push(pressed[i]);
         pressed = p;
     }
     
-    //Check pressed against defined shortcuts
-    function checkShorts(e) {
+    function checkShortcuts(e) {
+	    //Check / execute shortcuts matching pressed arr
         for(var i = 0, l = shortcuts.length; i < l; i++) {
             var keys = shortcuts[i].keys;
             if(arraysEqual(keys, pressed)) {
@@ -82,16 +61,10 @@ var Kees = (function() {
         }
     }
     
-    //Compare pressed array and shortcut array
     function arraysEqual(a, b) {
-        if(!a || !b) return false;
-        if(a.length !== b.length) return false;
-        for(var i = 0, l = a.length; i < l; i++) {
-            if(a[i] !== b[i]) return false;
-        }
-        return true;
+        return (!!a && !!b) && a.toString() === b.toString();
     }
 
-    return { add: add };
+    return { add: add, remove: remove };
 
 })();
