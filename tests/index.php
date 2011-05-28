03cf60732c0a36f114d7d2dfbbb0ab7fa2e9de73
dstokes <doggee7@gmail.com>
2011-05-27 22:31:39 -0700

    add a minified version, of course

--- tests/index.php
+++ tests/index.php
@@ -4,13 +4,13 @@
  <body>
  <p id="out"></p>
  
- <script type="text/javascript" src="../src/kees.js"></script>
+ <script type="text/javascript" src="../src/kees.min.js"></script>
  <script type="text/javascript">
 	 Kees.add('ctrl+a', function() { 
 		 document.getElementById('out').innerHTML += 'Pressed ctrl+a<BR />';
 	 });
 	 Kees.add('ctrl+r', function() {
-         Kees.remove('ctrl+a', 'down');
+		Kees.remove('ctrl+a', 'down');
 	 });
  </script>
  </body>
